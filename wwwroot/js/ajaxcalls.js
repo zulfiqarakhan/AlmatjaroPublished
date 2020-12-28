24362594faf0e915e3c2bad5d3dfc3a121a87bc4
zulfiqarakhan <76686031+zulfiqarakhan@users.noreply.github.com>
2020-12-28 15:34:52 +0500

    1st publish 28-12-2020
    1st publish 28-12-2020

---
+++ wwwroot/js/ajaxcalls.js
@@ -0,0 +1,30 @@
+﻿var $ = jQuery.noConflict();
+
+var KAY = KAY || {};
+(function ($) {
+    "use strict";
+    KAY.Cart = {        
+        init: function () {
+            KAY.Cart.getTopCart();
+		},
+        getTopCart: function () {
+            $.ajax({
+                url: "/Cart/TopCart",
+                type: "GET"
+            }).done(function (response) {
+                $topCart.empty().html(response);
+            });
+        }
+    };
+    KAY.documentOnReady = {
+		init: function () {
+            KAY.Cart.init();
+		}
+    };
+
+
+
+    var $topCart = $('#top-cart'),
+          $sadfa = $('#top-f');
+    $(document).ready(KAY.documentOnReady.init);
+})(jQuery); 
---
+++ wwwroot/js/site.js
@@ -0,0 +1,56 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var InitDataTable = function (selector) {
+    $(selector).DataTable();
+
+
+}
+
+var SwalDelete = function (Id, Url, refreshList) {
+    swal.fire({
+        title: "Delete?",
+        text: "Please ensure and then confirm!",
+        type: "warning",
+        showCancelButton: !0,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: !0
+    }).then(function (e) {
+        if (e.value === true) {
+            $.ajax({
+                type: 'POST',
+                url: Url,
+                data: { Id: Id },
+                //dataType: 'JSON',
+                success: function (results) {
+                    if (results.success === true) {
+                        swal.fire("Done!", results.message, "success");
+                        if (refreshList != null && refreshList != undefined) {
+                            refreshList();
+                        }
+                    } else {
+                        swal.fire("Error!", results.message, "error");
+                    }
+                }
+            });
+
+        } else {
+            e.dismiss;
+        }
+    }, function (dismiss) {
+        return false;
+    })
+};
+var RefreshPage = function () {
+    setTimeout(function () {
+        location.reload();
+    },1500);   
+};
+
+var isNullOrEmpty = function (value) {
+    return !(typeof value === "string" && value.length > 0);
+}
+
